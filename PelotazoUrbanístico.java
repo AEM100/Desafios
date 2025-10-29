0056acf010ba6fef278fec46be5f84f3a0d1df62
Anuar Moraber Bardouni <93945130+AEM100@users.noreply.github.com>
2025-10-29 10:48:56 +0100

    Rename class Pelotazo to PelotazoUrbanístico

--- PelotazoUrbanístico.java
+++ PelotazoUrbanístico.java
@@ -1,15 +1,15 @@
-"""
+/*
   https://aceptaelreto.com/problem/statement.php?id=755
 Moisés Eña está dispuesto a hacerse rico gracias al ladrillo. Ha comprado un gran terreno en un pueblecito bien comunicado y planea hacer una urbanización de lujo. 
 Hace algún tiempo intentó forrarse haciendo pisos baratos para gente con recursos modestos y el tema no acabó muy bien.
 
 Ahora ha puesto su punto de mira en gente más acaudalada y cree que la mejor forma de llamar su atención es haciendo viviendas unifamiliares en parcelas lo más grandes posible. 
 Por eso quiere dividir su terreno en el menor número de parcelas posible, para que sean cuadradas y tan grandes como se pueda, 
-siempre que todas sean iguales y cubran el terreno completo sin dejar espacio sin utilizar."""
+siempre que todas sean iguales y cubran el terreno completo sin dejar espacio sin utilizar.*/
 
 import java.util.Scanner;
 
-public class Pelotazo {
+public class PelotazoUrbanístico {
 
     public static long gcd(long a, long b) {
         return (b == 0) ? a : gcd(b, a % b);
